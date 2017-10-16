c8ab26d75c255ce3e017c98e45a881c814993b8b
zuenaj27 <zuenaj27@gmail.com>
2017-10-16 23:00:06 +0300

    first second commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
           <div className="App">
             <header className="App-header">
 
-              <img src={logo} className="App-logo" alt="logo" />
+             
               <h1 className="App-title">Welcome to Farmer</h1>
             </header>
               <div>
